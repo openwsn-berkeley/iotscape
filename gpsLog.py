50fedaa765908d8fc241d782891b56fe929ce3f7
Camilolopezlop <camilo-andres.lopez-lopez@inria.fr>
2019-07-18 18:20:01 +0200

    gpsLog.py updated

--- gpsLog.py
+++ gpsLog.py
@@ -4,12 +4,13 @@ FILENAME_OUTPUT = 'gpsData.txt'
 PORT            = 'COM28'
 
 with open(FILENAME_OUTPUT, "a") as outfile:
-    try:
-        s = serial.Serial(PORT,9600)
-        while True:    
-            line = s.readline().decode('utf-8')
-            outfile.write(line)
-            
-    except IOError:
-        print "Could not read from serial port"
+    while True:
+        try:
+            s = serial.Serial(PORT,9600)
+            while True:    
+                line = s.readline().decode('utf-8')
+                outfile.write(line)
+                
+        except IOError:
+            print "Could not read from {0}".format(PORT)
         
