fa378fa90466628bfe2823166e121c291b8a545e
Camilolopezlop <camilo-andres.lopez-lopez@inria.fr>
2019-07-24 17:38:34 +0200

    #4 Fix

--- postprocessing.py
+++ postprocessing.py
@@ -1,101 +1,148 @@
 # Python
 import time
 import struct
-import threading
-import Queue
-import traceback
-import os
+import glob
 # third-party
-
 
 #============================ helpers =========================================
 
+#============================ defines =========================================
 
 #============================ classes =========================================
 
-class Postprocessing():
+class GpsLog():
+
+    def __init__(self):
+        
+        # local variables
+        self.gpsData       = []
+        
+        # parse GPS log files
+        for filename in glob.glob('*.log'):
+            print 'parsing {0}'.format(filename)
+            with open(filename, 'r') as f:
+                for line in f:
+                    if line.startswith('$GPRMC'):
+                        
+                        # parse
+                        elems          = line.split(',')
+                        nowTime        = elems[1]
+                        lat            = elems[3]
+                        lat_heading    = elems[4]
+                        lon            = elems[5]
+                        lon_heading    = elems[6]
+                        nowDate        = elems[9]
+                        
+                        # skip line if no position acquired
+                        if lat=='':
+                            continue
+                        
+                        # timestamp
+                        ts_string      = int(nowDate+nowTime.split('.')[0])
+                        ts_string      = '{0} GMT'.format(ts_string)
+                        timestamp      = int(time.mktime(time.strptime(ts_string, '%y%m%d%H%M%S %Z'))+3600)
+                        
+                        
+                        # location
+                        location       =  self._computeLatLon(lat,lat_heading,lon,lon_heading)
+                        
+                        # store
+                        self.gpsData  += [(timestamp,location)]
+
+    #======================== public ==========================================
+
+    def getLocationAtTs(self,ts):
+        for (timestamp,location) in self.gpsData:
+            if timestamp == ts:
+                return location
+    
+    #======================== private =========================================
+    
+    def _computeLatLon(self,lat,lat_heading,lon,lon_heading):
+        outlat          = int(lat[:2])+float(lat[2:])/60
+        if lat_heading == 'S':
+            outlat      = -outlat
+        outlon          = int(lon[:3])+float(lon[3:])/60
+        if lon_heading == 'W':
+            outlon      = -outlon
+        return (outlat,outlon)
+
+class PcapLog(object):
     
     # global variables
-    OFFSET_PACKET_DESTINATION          = 22
     OFFSET_PACKET_LATITUDE             = 11
     OFFSET_PACKET_LONGITUDE            = 15
     PCAP_GLOBALHEADER_LEN              = 24 # 4+2+2+4+4+4+4
     PCAP_PACKETHEADER_LEN              = 16 # 4+4+4+4
     BEAMLOGIC_HEADER_LEN               = 20 # 1+8+1+1+4+4+1
-    BEAMLOGIC_OFFSET_TIME              = 3600
     PCAP_FILES_PATH                    = 'C:\Users\clopezlo\Desktop\iotscapeData' 
-    NOISY_MOTE_DESTINATION             = '\x9d\x9d'
     FILENAME_OUTPUT                    = 'processed_packets.pcap'
+    EXTRACT_MY_BEACON                  = ''.join([chr(b) for b in [157]*19])
 
-    def __init__(self, gps):
+    def __init__(self, gpslog):
     
         # store params
-        self.gps             = gps
+        self.gpslog                    = gpslog
 
         # local variables
-        self.rxBuffer                  = []
-        self.packet                    = []
-        self.globalHeader              = []
-        self.doneReceivingGlobalHeader = False
-        self.doneReceivingPacketHeader = False
-        self.doneWritingGlobalHeader   = False
         self.outfile                   = open(self.FILENAME_OUTPUT,'wb')
-
-        # start the class
-        self.name                      = 'Postprocessing'
-        self.run()
-    
-    def run(self):
         
-        # read data from all pcap files
-        for file in os.listdir(self.PCAP_FILES_PATH):
-            if file.endswith('.pcap'):
-                self.filePath   = os.path.join(self.PCAP_FILES_PATH, file)
-                with open(self.filePath, 'rb') as fileInProcess:
-                    print 'Processing {0}'.format(self.filePath)
+        with open(self.FILENAME_OUTPUT,'wb') as fout:
+            # read data from all pcap files
+            doneWritingGlobalHeader   = False
+            for filename in glob.glob('2*.pcap'):
+                print 'parsing {0}'.format(filename)
+                with open(filename, 'rb') as f:
+                    # global header
+                    globalHeader = f.read(self.PCAP_GLOBALHEADER_LEN)
+                    
+                    # write global header                   
+                    if not doneWritingGlobalHeader:
+                        fout.write(globalHeader)
+                        doneWritingGlobalHeader = True
+                        
                     while True:
                         try:
-                            b = ord(fileInProcess.read(1))
+                            # read header
+                            h  = f.read(self.PCAP_PACKETHEADER_LEN)
+                            ph = self._parsePcapPacketHeader(h)
+                            
+                            # read packet
+                            p = f.read(ph['incl_len'])
                         except:
                             break
-                        self._newByte(b) 
-                    
-                    # close file and reset variables
-                    fileInProcess.close()
-                    self.doneReceivingGlobalHeader = False
-                    self.doneReceivingPacketHeader = False
-                    self.rxBuffer                  = []
+                        
+                        # skip incomplete packets
+                        if ph['incl_len']!=len(p):
+                            continue
+                            
+                        # skip our own beacons
+                        if p[21:21+19]==self.EXTRACT_MY_BEACON:
+                            continue
+                        
+                        # get location
+                        ts = ph['ts_sec']-3600
+                        ts1 = min([i[0] for i in self.gpslog.gpsData], key=lambda x:abs(x-ts))
+                        location = self.gpslog.getLocationAtTs(ts1)
+                        if abs(ts1-ts)>120:
+                            print "WARNING: Nearest GPS signal with {0} seconds difference".format(abs(ts1-ts))
+                        
+                        p = [ord(b) for b in p]
+                        
+                        # write location
+                        p[11:11+4] = [ord(b) for b in struct.pack('>f',location[0])]
+                        p[15:15+4] = [ord(b) for b in struct.pack('>f',location[1])]
+
+                        # write header
+                        fout.write(h)
+                        
+                        # write packet
+                        fout.write(''.join([chr(b) for b in p]))
 
     #======================== public ==========================================
 
     #======================== private =========================================
-    def _newByte(self, b):
-        
-        self.rxBuffer += [b]
-        
-        # PCAP global header
-        if   not self.doneReceivingGlobalHeader:
-            if len(self.rxBuffer) == self.PCAP_GLOBALHEADER_LEN:
-                self.doneReceivingGlobalHeader       = True
-                self.globalHeader                    = self.rxBuffer
-                self.rxBuffer                        = []
-
-        # PCAP packet header
-        elif not self.doneReceivingPacketHeader:
-            if len(self.rxBuffer) == self.PCAP_PACKETHEADER_LEN:
-                self.doneReceivingPacketHeader       = True
-                self.packetHeader                    = self._parsePcapPacketHeader(self.rxBuffer)
-                assert self.packetHeader['incl_len'] == self.packetHeader['orig_len']
-                self.packet                          = self.rxBuffer
-                self.rxBuffer                        = []
-                
-        # PCAP packet bytes
-        else:
-            if len(self.rxBuffer) == self.packetHeader['incl_len']:
-                self.doneReceivingPacketHeader       = False
-                self._newFrame(self.rxBuffer)
-                self.rxBuffer                        = []
-                
+    
     def _parsePcapPacketHeader(self, header):
         """
         Parse a PCAP packet header
@@ -116,105 +163,16 @@ class Postprocessing():
             returnVal['ts_usec'],
             returnVal['incl_len'],
             returnVal['orig_len'],
-        ) = struct.unpack('<IIII', ''.join([chr(b) for b in header]))
+        ) = struct.unpack('<IIII', header)
 
         return returnVal
-
-    def _newFrame(self, frame):
-        
-        self.packetDestination    = ''.join([chr(b) for b in frame[self.OFFSET_PACKET_DESTINATION:self.OFFSET_PACKET_DESTINATION+2]])
-        if self.packetDestination != self.NOISY_MOTE_DESTINATION:
-            self.timestamp        = int(self.packetHeader['ts_sec'])-self.BEAMLOGIC_OFFSET_TIME 
-            while True:
-                try:
-                    self.latitude, self.longitude     = self.gps.getLocation(self.timestamp)
-                    break
-                except:
-                    self.timestamp                    += 1
-            self.writeBytes(frame)
-
-    def writeBytes(self, frame):
-        
-        # write global header
-        if   not self.doneWritingGlobalHeader:
-            self.outfile.write(''.join([chr(b) for b in self.globalHeader]))
-            self.doneWritingGlobalHeader = True
-        
-        # convert to bytes
-        bytesHeader        = ''.join([chr(b) for b in self.packet])
-        bytesPacketInitial = ''.join([chr(b) for b in frame[:self.OFFSET_PACKET_LATITUDE]])
-        bytesLatitude      = ''.join([chr(b) for b in [40,0,0,0]])
-        bytesLongitude     = ''.join([chr(b) for b in [40,0,0,0]])
-        bytesPacketFinal   = ''.join([chr(b) for b in frame[self.OFFSET_PACKET_LATITUDE+8:]])
-        bytesFrame         = bytesHeader+bytesPacketInitial+bytesLatitude+bytesLongitude+bytesPacketFinal
-        
-        # write bytes 
-        self.outfile.write(bytesFrame)
-
-class Gps():
-
-    # global variables
-    GPS_FILES_PATH         = 'C:\Users\clopezlo\Desktop\iotscapeData'
-    GPS_OFFSET_TIME        = 7200 # 2 hours (3600s*2)
-    
-    def __init__(self):
-        
-        # local variables
-        self.gpsData       = []
-        
-        # start the class
-        self.name          = 'Gps'
-        self.run()
-
-    def run(self):
-        
-        # list: gpsData (timestamp, latitude, longitude)
-        for file in os.listdir(self.GPS_FILES_PATH):
-            if file.endswith('.log'):
-                self.filePath    = os.path.join(self.GPS_FILES_PATH, file)
-                self.date        = file.split('-')[0]
-                with open(self.filePath, 'r') as fileInProcess:
-                    for line in fileInProcess:
-                        data = line.split(",")
-                        if data[0] == "$GPGLL":
-                            if data[1]=='':
-                                continue
-                            self.timestamp, self.latitude, self.longitude = self.getGpsInfo(data)
-                            self.gpsData.append([self.timestamp, self.latitude, self.longitude])
-                            
-    #======================== public ==========================================
-
-    #======================== private =========================================
-    def getGpsInfo(self, data):
-        lat         = data[1]
-        lat         = int(lat[:2])+float(lat[2:])/60
-        latDir      = data[2]
-        long        = data[3]
-        long        = int(long[:3])+float(long[3:])/60
-        longDir     = data[4]
-
-        if latDir == 'S':
-            lat     = -lat
-
-        if longDir == 'W':
-            long    = -long
-            
-        gpsTime     = str(data[5].split(".")[0])
-        dateTime    = self.date + gpsTime
-        timestamp   = time.mktime(time.strptime(dateTime, '%Y%m%d%H%M%S'))+ self.GPS_OFFSET_TIME
-        return timestamp, lat, long
-
-        
-    def getLocation(self, timestamp):
-        for row in self.gpsData:
-            if row[0] == timestamp:
-                return row[1], row[2]
 
 #============================ main ============================================
 
 def main():    
     # start 
-    gps                      = Gps()
-    postprocessing           = Postprocessing(gps)
+    gpslog    = GpsLog()
+    pcapLog   = PcapLog(gpslog)
+
 if __name__ == "__main__":
     main()
